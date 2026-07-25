439cff6a40a3fb2c6674ee60ae23925101979e4b
Thebigdriz001 <ibrahimshamsuddeenkhalil18@gmail.com>
2026-07-26 00:29:57 +0100

    Add descriptive comments to variables.js

--- BASICS/LAB-01/code/variables.js
+++ BASICS/LAB-01/code/variables.js
@@ -1,38 +1,23 @@
-// LAB-01: Variables and Data Types
-// Starter Code
-
-// ==========================================
-// 1. Personal Information Variables
-// ==========================================
-
-// TODO: Declare a variable called 'myName' and set it to your name (string)
-// Hint: use const because your name probably won't change!
-const myName = "Your Name Here";
-
-// TODO: Declare a variable called 'myAge' and set it to your age (number)
-// Hint: use let because your age changes every year!
-
-
-// TODO: Declare a variable called 'isStudent' and set it to true or false (boolean)
-
-
-// TODO: Declare a variable called 'favoriteLanguage' and set it to your favorite programming language (string)
-
-
-// ==========================================
-// 2. Output to Console
-// ==========================================
-
-// Here is an example of how to log your name:
-console.log("Name:", myName);
-
-// TODO: Use console.log() to output your age, student status, and favorite language
-// Make sure to include descriptive labels like the example above!
-
-
-// ==========================================
-// 3. Challenge (Optional)
-// ==========================================
-// Try to log a full sentence using your variables, like:
-// "Hi, I am Alice. I am 25 years old. It is true that I am a student."
-
+// Personal information
+// Store the user's full name in a constant variable
+// 'const' is used because the name will not change.
+const myName = "Ibrahim Shamsuddeen";
+// Display the user's name in the console.
+console.log("Name: ", myName);
+// Store the user's age in a variable.
+// 'let' is used because the value can be changed later if needed.
+let myAge = 20;
+// Display the user's age in the console.
+console.log("Age: ", myAge);
+// Store whether the user is a student in a variable.
+// 'let' is used because the value can be changed later if needed.
+let isStudent = true;
+// Display whether the user is a student in the console.
+console.log("Is Student: ", isStudent);
+// Store the user's favourite programming language in a variable.
+// 'let' is used because the value can be changed later if needed.
+let FavoriteProgrammingLanguage = "Python";
+// Display the user's favourite programming language in the console.
+console.log("Favourite Programming Language: ", FavoriteProgrammingLanguage);  
+// Display a message with all the user's information.
+console.log("Hi, my name is " + myName + ". I am " + myAge + " years old, and I am a student. My favourite programming language is " + FavoriteProgrammingLanguage);
