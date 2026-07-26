28644c39686c547f904b1d14937f208e49d45b6a
Thebigdriz001 <ibrahimshamsuddeenkhalil18@gmail.com>
2026-07-26 22:27:55 +0100

    Complete TICKET-01: Calculator

---
+++ BASICS/TICKET-01/code/calculator.js
@@ -0,0 +1,9 @@
+// Store the first number in a variable.
+let num1 = 0.5;
+// Store the second number in a variable.
+let num2 = 1.5;
+// Perform calculations and display results.            
+console.log("Addition: ", num1 + num2);
+console.log("Subtraction: ", num1 - num2);
+console.log("Multiplication: ", num1 * num2);
+console.log("Division: ", num1 / num2);
